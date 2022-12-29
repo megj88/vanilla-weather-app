78406cdfcb3352f7471500d8750b4869ea117125
Megan <115540207+megj88@users.noreply.github.com>
2022-12-29 17:56:40 +0100

    Added date

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
+// Get date
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+//Display Current Weather
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let description = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(
     response.data.daily[0].temperature.day
   );
@@ -11,7 +37,9 @@ function displayTemperature(response) {
   description.innerHTML = response.data.daily[0].condition.description;
   humidityElement.innerHTML = response.data.daily[0].temperature.humidity;
   windElement.innerHTML = Math.round(response.data.daily[0].wind.speed);
+  dateElement.innerHTML = formatDate(response.data.daily[0].time * 1000);
 }
 
-let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=Lisbon&key=o7ea0936527a0d6te1ab2cfbbc475e37&units=metric`;
+let city = "Bari";
+let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=o7ea0936527a0d6te1ab2cfbbc475e37&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
