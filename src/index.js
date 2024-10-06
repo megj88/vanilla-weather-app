49e72ad0f7ff8605db543517279f36d25e8aa737
Megan Jones <115540207+megj88@users.noreply.github.com>
2024-10-06 13:52:58 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -28,15 +28,7 @@ function formatDay(timestamp) {
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
   return days[day];
-}
 
-function formatDay(timestamp) {
-  let date = new Date(timestamp * 1000);
-  let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-  return days[day];
-}
 
 function displayForecast(response) {
   let forecast = response.data.daily;
